96fc0ead1934969657c97782e6db104168d9b4ef
unknown <pierodavilaaguirre22@gmail.com>
2023-09-15 00:03:23 -0500

    Updates: login controller implemented

--- src/modules/auth/controllers/login.controller.ts
+++ src/modules/auth/controllers/login.controller.ts
@@ -4,7 +4,6 @@ import { Request, Response } from "express";
 
 
 export class LoginController extends BaseController {
-
     protected async response(req: Request, res: Response): Promise<any> {
         try {
             this.ok(res, 'everything its fine')
--- src/modules/auth/routes/auth.routes.ts
+++ src/modules/auth/routes/auth.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { LoginController } from "../controllers/login.controller";
 
+const loginController = new LoginController();
+
 const authRouter = Router();
 authRouter.post('/register', (req, res) => {
     res.send('register')
 });
 
 authRouter.post('/login', (req, res) => {
-    const loginController = new LoginController();
     loginController.execute(req, res);
 });
 
